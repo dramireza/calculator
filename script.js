a9e57129324e92e229af9cbd3ca30b5e207e69f5
David <>
2022-12-08 14:07:41 -0400

    very basic functionality, struggling a bit with logic

---
+++ script.js
@@ -0,0 +1,58 @@
+function add(a, b) {
+    return a + b;;
+}
+function substract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+
+const total = document.querySelector('.total');
+const subtotal = document.querySelector('.subtotal');
+let var1 = 0;
+let var2 = 0;
+let varop = '';
+let vartotal = 0;
+
+function operate() {
+    var2 = Number(total.textContent);
+
+    if (varop == '+') vartotal = add(var1, var2);
+    if (varop == '-') vartotal = substract(var1, var2);
+    if (varop == '*') vartotal = multiply(var1, var2);
+    if (varop == '/') vartotal = divide(var1, var2);
+
+
+    let prev = subtotal.textContent;
+    printToDisplay('subtotal', prev + var2)
+    printToDisplay('total', vartotal)
+    var1 = vartotal;
+    varop = '';
+    var2 = 0;
+}
+
+
+function number(a) {
+    let prev = total.textContent;
+    printToDisplay('total', `${prev}${a}`);
+}
+
+function cls() {
+    printToDisplay('total', '');
+}
+
+function operator(op) {
+    var1 = Number(total.textContent);
+    varop = op;
+    printToDisplay('subtotal', `${var1}${op}`)
+    cls();
+}
+
+function printToDisplay(display, toPrint) {
+    const tmp = document.querySelector('.' + display);
+    tmp.textContent = toPrint
+}
